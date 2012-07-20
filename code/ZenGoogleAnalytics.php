706ce7ca3d844b7ca2748c3f5a3b5a2e554cc207
sheadawson <shea@livsource.co.nz>
2012-07-20 23:53:17 +1000

    simplified and debugged untested code

--- code/ZenGoogleAnalytics.php
+++ code/ZenGoogleAnalytics.php
@@ -5,12 +5,6 @@ class ZenGoogleAnalytics extends SiteTreeDecorator {
 	public static $ga_id;
 
 	function GAID() {
-		if(Director::isLive() self::) {
-			return self::$ga_id;
-		}
-	}
-
-	public function forTemplate(){
-		return $this->renderWith(array('ZenGoogleAnalytics'));
+		return self::$ga_id;
 	}
 }
